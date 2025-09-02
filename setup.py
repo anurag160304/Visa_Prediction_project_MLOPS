e0ab5259942c136483da2e8090f8c2df99c69ad2
anurag <anuragbaghel9058@gmail.com>
2025-09-02 22:52:07 +0530

    structure is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anurag Baghel",
+    author_email="anuragbaghel9058@gmail.com",
+    packages=find_packages()
+)
